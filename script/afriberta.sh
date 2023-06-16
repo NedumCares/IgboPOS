53ee5d84349e3617c0a95602b2070c8b0c0c7add
Ignatius Ezeani <igezeani@yahoo.com>
2023-06-16 15:43:38 +0100

    modify script folder

--- script/afriberta.sh
+++ script/afriberta.sh
@@ -1,10 +1,10 @@
-for LANG in bam bbj ewe fon hau ibo kin lug luo nya pcm sna swa twi wol xho yor zul
+for LANG in ibo
 do
 	for j in 1 2 3 4 5
 	do
 		export MAX_LENGTH=200
-		export BERT_MODEL=bert-base-multilingual-cased
-		export OUTPUT_DIR=baseline_models/${LANG}_mbert
+		export BERT_MODEL=castorini/afriberta_large
+		export OUTPUT_DIR=baseline_models/${LANG}_afriberta
 		export TEXT_RESULT=test_result$j.txt
 		export TEXT_PREDICTION=test_predictions$j.txt
 		export BATCH_SIZE=16
@@ -12,8 +12,8 @@ do
 		export SAVE_STEPS=10000
 		export SEED=$j
 
-		CUDA_VISIBLE_DEVICES=1 python3 ../train_pos.py --data_dir ../data/${LANG}/ \
-		--model_type bert \
+		CUDA_VISIBLE_DEVICES=3 python3 ../train_pos.py --data_dir ../data/${LANG}/ \
+		--model_type xlmroberta \
 		--model_name_or_path $BERT_MODEL \
 		--output_dir $OUTPUT_DIR \
 		--test_result_file $TEXT_RESULT \
